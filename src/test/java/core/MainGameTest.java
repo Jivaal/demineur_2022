c1c69dbe5ee36051c2ff14b7fc28b3b99bf45b95
Kélean Gallet <kelean.gallet@gmail.com>
2022-04-18 19:50:06 +0200

    Création de la grille

--- src/test/java/core/MainGameTest.java
+++ src/test/java/core/MainGameTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class MainGameTest
 {
