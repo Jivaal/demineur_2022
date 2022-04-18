80904a88e372a8d6bdf0e1e39d7c717515e1a60c
Kélean Gallet <kelean.gallet@gmail.com>
2022-04-18 20:04:15 +0200

    Ecriture de la grille sous forme de singleton

--- src/main/java/mine/Grid.java
+++ src/main/java/mine/Grid.java
@@ -1,6 +1,45 @@
 package mine;
 
+import java.util.ArrayList;
+
 public class Grid
 {
+    public final int WIDTH = 64;
+    public final int HEIGHT = 64;
 
+    private ArrayList<ArrayList<Mine>> grid;
+
+    private static Grid instance = null;
+
+    private Grid()
+    {
+        fillGrid();
+    }
+
+    public static Grid getInstance()
+    {
+        if (Grid.instance == null)
+            Grid.instance = Grid.resetInstance();
+
+        return Grid.instance;
+    }
+
+    public static Grid resetInstance()
+    {
+        Grid.instance = new Grid();
+        Grid.instance.fillGrid();
+
+        return Grid.getInstance();
+    }
+
+    private void fillGrid()
+    {
+        this.grid = new ArrayList<ArrayList<Mine>>();
+        for (int i = 0; i < this.WIDTH; i++)
+        {
+            this.grid.add(new ArrayList<Mine>());
+            for (int j = 0; j < Grid.HEIGHT; j++)
+                grid.get(i).add(new Mine());
+        }
+    }
 }
