7a95feb2dd244e54e7f6fb27b473e2e79d59d324
Kélean Gallet <kelean.gallet@gmail.com>
2022-04-18 20:12:56 +0200

    Classe de test de la grille remplit avec les briques de base

--- src/test/java/mine/GridTest.java
+++ src/test/java/mine/GridTest.java
@@ -1,6 +1,19 @@
 package mine;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 public class GridTest
 {
+    private Grid grid;
 
+    @BeforeEach
+    public void init() { this.grid = Grid.getInstance(); }
+
+    @AfterEach
+    public void clear()
+    {
+        this.grid = Grid.resetInstance();
+        this.grid = null;
+    }
 }
